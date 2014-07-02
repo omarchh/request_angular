9a69d01eaa2914d6bfdbb34e42eab654017ad8a3
Omar <omaralex131191@gmail.com>
2014-07-02 11:32:06 -0500

    CHANGES: - Added bootstrap.min.css, reset_v2.0.css in css/ folder - Deleted unused files js/tweet.js, js/twitterfeed.js - Modified file get-tweets.php to get tweets from other account - Modified index.php for displaying tweets in a better way

--- get-tweets.php
+++ get-tweets.php
@@ -1,27 +1,24 @@
 <?php
 require_once("oauth/twitteroauth.php"); //Path to twitteroauth library
 
-//Conversion de contenidos
+//Content conversion
 $postdata = file_get_contents("php://input");
 $request = json_decode($postdata);
 $usuario = $request->user;
 $password = $request->pass;
 
-$twitteruser = "FutApp";//Username
-$notweets = 5;//number of tweets to get
+$twitteruser = "Om4rChH";//Username
+$notweets = 10;//number of tweets to get
+//Keys provided by twitter account
 $consumerkey = "LHkKJItKmJGj9EewF2543SQoY";
 $consumersecret = "cVImsvPmHZxrLte2JgCtUqx1JgNJApMvd4RzS99UgFVRB0V4C8";
 $accesstoken = "379872175-t4gLqlkgZ5wdP5d3A3AujfRwc8ZMU7Oq3yn9ptZ5";
 $accesstokensecret = "SR7GqQOoBJWVS6qmHMIU8iDCjwylgAHHvDVlHWIZ9leno";
- 
-function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
-  $connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
-  return $connection;
-}
-  
-$connection = getConnectionWithAccessToken($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
- 
+
+//getting conection to twitter
+$connection = new TwitterOAuth($consumerkey, $consumersecret, $accesstoken, $accesstokensecret);
+//getting tweets
 $tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
- 
+//encoding to JSON
 echo json_encode($tweets);
 ?>
--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <html ng-app="request">
 	<head>
 		<title>Requests</title>
+		<link rel="stylesheet" type="text/css" href="css/reset_v2.0.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	</head>
 	<body>
-
-		<div class="section-page-landing" id="twitter">
-			<div class="inner-section">
-				<div class="container text-center" id="tweets">
-					<h2><span><a href="https://twitter.com/FutApp" target="_blank">@FutApp</a></span></h2>
+		<div ng-controller="TwitterController as twitter" class="container text-center">
+			<div id="twitter">
+				<div class="text-center" id="tweets">
+					<h2><span><a href="https://twitter.com/Om4rChH" target="_blank">@Om4rChH</a></span></h2>
 					<div class="text-center" id="twitter-feed"></div>
 				</div>
-			</div>
-		</div>
-
-		<div ng-controller="TwitterController as twitter">
-			<div>
-				<button ng-click="twitter.getTweets()">Get getTweets</button>
-			</div>
-			<div ng-repeat="tweet in twitter.tweets">
-				hola
+				<button ng-click="twitter.getTweets(twitter)" class="btn btn-info">Get Tweets from @Om4rChH</button>
+				<br>
+				<br>
+				<div ng-repeat="tweet in twitter.tweets" class="text-left well">
+					<h4>
+						<img src="{{tweet.user.profile_image_url}}"/>&nbsp;<strong ng-show="tweet.retweeted_status">{{tweet.retweeted_status.user.name}} -></strong>
+						{{tweet.user.name}}
+					</h4>
+					<br>
+					{{tweet.text}}
+					<br>
+					<small>{{tweet.created_at}}</small>
+				</div>
 			</div>
 		</div>
 
